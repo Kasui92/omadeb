e69dee336b760c68c57c9f221f4dfd36f9a874f2
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-08-31 08:05:21 +0200

    feat(migrations): add migrations for the 1.4.1 alignment

--- migrations/1777409457.sh
+++
@@ -1,3 +0,0 @@
-echo "Update npx wrappers to run through mise node@latest"
-
-source "$OMADEB_PATH/install/packaging/npx.sh"
---
+++ migrations/1786828035.sh
@@ -0,0 +1,3 @@
+echo "Update npm packages"
+
+source "$OMADEB_PATH/install/packaging/npm.sh"
