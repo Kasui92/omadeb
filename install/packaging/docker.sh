9bed2d141f75520cb1f077a7534d2c977a943855
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-09-02 23:05:06 +0200

    fix(install): update Docker source list format and ensure debian package are dropped

--- install/packaging/docker.sh
+++ install/packaging/docker.sh
@@ -1,10 +1,21 @@
-if [[ ! -f /etc/apt/sources.list.d/docker.list ]]; then
+if [[ ! -f /etc/apt/sources.list.d/docker.sources ]]; then
     [[ -f /etc/apt/keyrings/docker.asc ]] && sudo rm /etc/apt/keyrings/docker.asc
     sudo install -m 0755 -d /etc/apt/keyrings
-    sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/debian/gpg
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 fi
 
 sudo apt-get update
+
+# Remove any conflicting packages that may interfere with Docker installation
+omadeb-pkg-drop docker.io docker-compose docker-doc docker-buildx podman-docker containerd runc
+# Install Docker packages
 omadeb-pkg-add docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
